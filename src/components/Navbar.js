568df923f5217f5a3c54c41d27b5506f71a165cb
Lys Paul Lukuka <lyslukuka@gmail.com>
2025-01-28 11:26:54 -0600

    Making the website a reactive website. Added more styling to navbar. Created a global style sheet index.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="header">
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={click ? "nav-menu.active" : "nav-menu"}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -23,6 +26,13 @@ const Navbar = () => {
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "white" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "white" }} />
+        )}
+      </div>
     </div>
   );
 };
