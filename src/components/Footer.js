1fe5cb238eda399b85d6ce1ddb799cfaaedea958
Lys Paul Lukuka <lyslukuka@gmail.com>
2025-01-28 17:08:16 -0600

    Added a footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,72 @@
+import "./Footer.css";
+
+import React from "react";
+import {
+  FaFacebook,
+  FaHome,
+  FaInstagram,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>490 NW Compass Ave</p>
+              <p>Waukee, IA 50263</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              515-553-8785
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              lyslukuka@gmail.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About me</h4>
+          <p>
+            My name is Lys Paul Lukuka. I am a software Engineer passionate
+            about crafting clean, efficient, and user-centric solutions. With
+            expertise in full-stack development, enterprise app solution, I
+            thrive on turning ideas into reality through code.
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaInstagram
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
