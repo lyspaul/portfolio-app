9d61cd4bf3c641c991fa22e10a8bf971e1ed86fc
Lys Paul Lukuka <lyslukuka@gmail.com>
2025-01-28 16:14:19 -0600

    Added a background color and function to the navigation bar. Made the HeroImg reactive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,19 @@ import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
+
+  const [color, setColor] = useState(false);
+  //set the color to true if the window scroll is greater than 100 pixel, false if not
+  const changeColor = () => {
+    //using the window event listener
+    if (window.scrollY >= 100) {
+      setColor(true);
+    } else setColor(false);
+  };
+
+  window.addEventListener("scroll", changeColor);
   return (
-    <div className="header">
+    <div className={color ? "header header-bg" : "header"}>
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
