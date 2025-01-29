ca4ef17c68ac832d29ebab10939922a00eceb5a2
Lys Paul Lukuka <lyslukuka@gmail.com>
2025-01-28 19:09:56 -0600

    Added a global HeroSection css and jss. Worked on the project component

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import HeroImg from "../components/HeroImg";
+import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div>
+      <Navbar />
+      <HeroSection heading="PROJECTS" text="My most recent works" />
+      <Footer />
+    </div>
+  );
 };
 
 export default Project;
